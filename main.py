ce5b0dd04c2a149e4e7b51991b9491c4becf54be
todd <todd.develop@gmail.com>
2022-03-29 19:17:04 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,61 @@
-items = (1, 2, 3)
-it = iter(items)
-print(it)
-while True:
-    print(next(it))
+from abc import ABC, ABCMeta, abstractmethod
+from calendar import c
+
+
+class User(ABC):
+
+    @abstractmethod
+    def create():
+        pass
+    
+    @abstractmethod
+    def update():
+        pass
+    
+    @abstractmethod
+    def delete():
+        pass
+
+    @abstractmethod
+    def get():
+        pass
+
+
+class CustomUser(User):
+    
+    def create():
+        pass
+
+    def update():
+        pass
+
+    def get():
+        pass
+
+    def delete():
+        pass
+
+
+class OneMeta(ABCMeta):
+    """Абстрактный Мета класс"""
+    pass
+
+
+class Meta(type):
+    """Мета класс"""
+    def __new__(cls, name, bases, attrs):
+        attrs["my_field"] = "Value"
+        return super().__new__(cls, name, bases, attrs)
+
+
+class User(metaclass=Meta):
+    def __init__(self) -> None:
+        print("Конструктор")
+        super().__init__()
+
+
+
+u = User()
+print(u.my_field)
+
+
